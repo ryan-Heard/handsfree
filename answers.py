bc7b96362ce1f99b7fbb195dd220f2362e0057c0
Ryan Heard <brotherakuma@gmail.com>
2016-12-02 21:34:33 -1000

    minor

---
+++ answers.py
@@ -0,0 +1,14 @@
+
+
+def urban_dictinary(term):
+    #sland and hackerz terms
+    pass
+
+
+def wikipedia(term):
+    # quick page result.
+    pass
+
+
+def software_terms(term):
+    pass
---
+++ programmers.py
@@ -0,0 +1,19 @@
+
+def open_ssh():
+    pass
+
+
+def start_new_node():
+    # start new ngular fullstack project
+    pass
+
+
+def start_mongo_db():
+    pass
+
+
+def start_putty():
+    pass
+
+def open_atom(folder_name):
+    pass
